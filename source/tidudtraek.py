3364440f32baed00e9d13ba54f15eb9f220e51f1
Søren Rasmussen <soren.fussingo@gmail.com>
2023-12-17 15:48:34 +0100

    Initial commit

---
+++ source/tidudtraek.py
@@ -0,0 +1,75 @@
+from pathlib import Path  # core python module
+
+import pandas as pd  # pip install pandas openpyxl
+import PySimpleGUI as sg  # pip install pysimplegui
+
+# import af egne moduler
+from settingswindow import settings_window
+
+
+def main_window():
+    default_folder = settings["PROGRAM"]["outputfolder_default"]
+    # ------ GUI Definition ------ #
+    layout = [
+        [sg.Text("", s=20)],
+        [
+            sg.T("Input File:", s=15, justification="r"),
+            sg.I(s=60, key="-IN-"),
+            sg.FileBrowse(
+                initial_folder=default_folder, file_types=(("Excel Files", "*.xls*"),)
+            ),
+        ],
+        [
+            sg.T("Output Folder:", s=15, justification="r"),
+            sg.I(s=60, default_text=default_folder, key="-OUT-"),
+            sg.FolderBrowse(initial_folder=default_folder),
+        ],
+        [sg.Text("", s=20)],
+        [
+            sg.Text("", s=20),
+            sg.Exit(s=15, button_color="tomato"),
+            sg.B("Settings", s=15),
+            sg.B("Lav nyt Tidsudtræk", s=15),
+            sg.Text("", s=20),
+        ],
+    ]
+
+    window_title = " " * 80 + settings["GUI"]["title"] + " " * 80
+    window = sg.Window(window_title, layout, use_custom_titlebar=True)
+
+    while True:
+        event, values = window.read()
+        if event in (sg.WINDOW_CLOSED, "Exit"):
+            break
+        if event == "Settings":
+            window.disappear()
+            sg.popup_no_titlebar("Settings Ikke implementeret!")
+            # settings_window(settings)
+            window.reappear()
+            window.bring_to_front()
+
+        if event == "Lav nyt Tidsudtræk":
+            window.disappear()
+            sg.popup_no_titlebar("Tidsudtræk Ikke implementeret!")
+            # settings_window(settings)
+            window.reappear()
+            window.bring_to_front()
+
+    window.close()
+
+
+if __name__ == "__main__":
+    SETTINGS_PATH = Path.cwd()
+    # create the settings object and use ini format
+    settings = sg.UserSettings(
+        path=SETTINGS_PATH,
+        filename="tidudtraek.ini",
+        use_config_file=True,
+        convert_bools_and_none=True,
+    )
+    theme = settings["GUI"]["theme"]
+    font_family = settings["GUI"]["font_family"]
+    font_size = int(settings["GUI"]["font_size"])
+    sg.theme(theme)
+    sg.set_options(font=(font_family, font_size))
+    main_window()
